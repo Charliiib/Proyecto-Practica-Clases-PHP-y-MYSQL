75089507a91bb1739c6daa8f9de4b993d3443dd8
CharliB <barros.rcarlos@gmail.com>
2020-12-04 20:26:22 -0300

    add forms done

---
+++ formAgregarMarca.php
@@ -0,0 +1,25 @@
+<?php
+    require 'config/config.php';
+    require 'funciones/autenticar.php';
+        autenticar();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+<main class="container">
+    <h1>Alta de una nueva marca</h1>
+
+        <div class="bg-light border p-4">
+            <form action="agregarMarca.php" method="post">
+                Marca: <br>
+                    <input type="text" name="mkNombre" class="form-control"><br>
+                        <button class="btn btn-dark">Agregar marca</button>
+                            <a href="adminMarcas.php" class="btn btn-outline-secondary">Volver a panel</a>
+            </form>
+        </div>
+
+</main>
+
+<?php
+include 'includes/footer.php';
+?>
