53634dab50925c0f2c0283a90297e4bb51a15cef
CharliB <barros.rcarlos@gmail.com>
2020-12-04 19:44:32 -0300

    first commit

---
+++ template.php
@@ -0,0 +1,14 @@
+<?php  
+    include 'config/config.php';
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Tema de la página</h1>
+
+    </main>
+
+<?php  
+include 'includes/footer.php';  
+?>
