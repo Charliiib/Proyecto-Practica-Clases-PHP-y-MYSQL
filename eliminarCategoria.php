1f9c6bbc207fb6f09ee7d7475f899f040d5c9767
CharliB <barros.rcarlos@gmail.com>
2020-12-04 21:11:55 -0300

    check delets

---
+++ eliminarCategoria.php
@@ -0,0 +1,29 @@
+<?php  
+    require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/categorias.php';
+        $chequeo = eliminarCategoria();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+<main class="container">
+        <h1>Eliminacion de una Categoria</h1>
+<?php
+    $class = 'danger';
+    $mensaje = 'No se pudo eliminar la Categoria';
+    if( $chequeo ) {
+        $class = 'success';
+        $mensaje = 'Categoria eliminada correctamente';
+    }
+?>
+            <div class="alert alert-<?= $class ?>">
+                <?= $mensaje ?><br>
+                <a href="adminCategorias.php" class="btn btn-outline-secondary">Volver a panel</a>
+            </div>
+
+</main>
+
+<?php
+include 'includes/footer.php';
+?>
---
+++ eliminarMarca.php
@@ -0,0 +1,29 @@
+<?php  
+    require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+        $chequeo = eliminarMarca();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+<main class="container">
+        <h1>Eliminacion de una Marca</h1>
+<?php
+    $class = 'danger';
+    $mensaje = 'No se pudo eliminar la marca';
+    if( $chequeo ) {
+        $class = 'success';
+        $mensaje = 'Marca eliminada correctamente';
+    }
+?>
+            <div class="alert alert-<?= $class ?>">
+                <?= $mensaje ?><br>
+                <a href="adminMarcas.php" class="btn btn-outline-secondary">Volver a panel</a>
+            </div>
+
+</main>
+
+<?php
+include 'includes/footer.php';
+?>
