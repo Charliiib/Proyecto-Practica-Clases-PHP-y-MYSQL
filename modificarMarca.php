34938071167d0dab8227fc9607f76bc71b945d5e
CharliB <barros.rcarlos@gmail.com>
2020-12-04 21:07:31 -0300

    check modifications

---
+++ modificarMarca.php
@@ -0,0 +1,31 @@
+<?php
+    require 'config/config.php';
+    require 'funciones/autenticar.php';
+        autenticar();
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+        $chequeo = modificarMarca();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+<main class="container">
+    <h1>Modificación de una marca</h1>
+<?php
+    $class = 'danger';
+    $mensaje = 'No se pudo modificar la Marca';
+    if( $chequeo ) {
+        $class = 'success';
+        $mensaje = 'Marca modificada correctamente';
+    }
+?>
+            <div class="alert alert-<?= $class ?>">
+                <?= $mensaje ?> <br>
+                    <a href="adminMarcas.php" class="btn btn-outline-secondary">Volver a panel</a>
+            </div>
+
+    </main>
+
+<?php  
+include 'includes/footer.php';
+?>
