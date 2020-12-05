a53f063fb3f34792ca9dfc8ec1a2eece8ca7b5a8
CharliB <barros.rcarlos@gmail.com>
2020-12-04 21:02:39 -0300

    check additions

---
+++ agregarMarca.php
@@ -0,0 +1,32 @@
+<?php
+    require 'config/config.php';
+    require 'funciones/autenticar.php';
+        autenticar();
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+        $chequeo = agregarMarca();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+<main class="container">
+        <h1>Alta de una marca</h1>
+
+<?php
+    $class = 'danger';
+    $mensaje = 'No se pudo agregar la Marca';
+    if( $chequeo ) {
+        $class = 'success';
+        $mensaje = 'Marca agregada correctamente';
+    }
+?>
+            <div class="alert alert-<?= $class ?>">
+                <?= $mensaje ?> <br>
+                    <a href="adminMarcas.php" class="btn btn-outline-secondary">Volver a panel</a>
+            </div>
+
+</main>
+
+<?php
+include 'includes/footer.php';
+?>
